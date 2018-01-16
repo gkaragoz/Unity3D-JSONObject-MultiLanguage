088913f3eab75e70338bb287a602b481d59be75e
gkaragoz <gkaragoz96@gmail.com>
2018-01-16 12:44:50 +0300

    Add LanguageManager and UIManager. Finish Project

---
+++ MultiLanguage JSONObject/Assets/Scripts/LanguageManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageManager : MonoBehaviour {
+
+	public enum Language
+	{
+		TR,
+		EN
+	}
+
+	public Language currentLanguage;
+
+	private JSONObject data;
+
+	public void LoadLanguage(Language language) {
+		currentLanguage = language;
+
+		string path = language.ToString();
+		string languageFile = Resources.Load(path).ToString();
+		
+		data = new JSONObject(languageFile);
+	}
+
+	public string JSONClearify(JSONObject obj){
+		return obj.ToString().Replace("\"", "");
+	}
+
+	public string GetData(string field) {
+		JSONObject fieldData = data.GetField(field);
+		string str = JSONClearify(fieldData);
+
+		if(fieldData == null)
+			return "";
+		
+		return str;
+	}
+}
---
+++ MultiLanguage JSONObject/Assets/Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	public Button btnTurkish;
+	public Button btnEnglish;
+	public Text txtWelcome;
+	public Text txtVersion;
+
+	private LanguageManager languageManager;
+
+	void Start()
+	{
+		languageManager = GetComponent<LanguageManager>();
+		SetButtonDelegates();
+		SelectLanguage(languageManager.currentLanguage);
+	}
+
+	void SetButtonDelegates() {
+		btnTurkish.onClick.AddListener(delegate {
+			SelectLanguage(LanguageManager.Language.TR);
+		});
+		btnEnglish.onClick.AddListener(delegate {
+			SelectLanguage(LanguageManager.Language.EN);
+		});
+	}
+
+	void SetUITexts() {
+		txtWelcome.text = languageManager.GetData("txtWelcome");
+		txtVersion.text = languageManager.GetData("txtVersion") + ": " + languageManager.currentLanguage;
+	}
+
+	public void SelectLanguage(LanguageManager.Language language) {
+		languageManager.LoadLanguage(language);
+		SetUITexts();
+	}
+
+}
